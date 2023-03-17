fcff74dd5f5f36aaeef5f4919c72280f35a84bd8
Motonosuke <67644458+Motonosuke@users.noreply.github.com>
2023-03-17 16:45:33 +0900

    refactor:footer

--- src/components/Layouts/MainLayout.tsx
+++ src/components/Layouts/MainLayout.tsx
@@ -1,13 +1,13 @@
 import type { CustomLayout } from 'next';
 
-import { Header } from 'components/Header';
+import { MainHeader } from 'components/Header';
 
 // const HEADER_HEIGHT = 64;
 
 export const MainLayout: CustomLayout = (page) => {
   return (
     <>
-      <Header />
+      <MainHeader />
       <main>
         <div className="mx-auto mt-10 w-4/5">{page}</div>
       </main>
