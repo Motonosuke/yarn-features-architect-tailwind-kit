392c24df310d1e304f1d1152cdc3e6e326ba67c7
Motonosuke <67644458+Motonosuke@users.noreply.github.com>
2023-04-29 19:55:45 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  i18n: { locales: ['ja'], defaultLocale: 'ja' },
   reactStrictMode: true,
   experimental: {
     esmExternals: false,
