127b5bcfac9194a62e518ae690cf10405d335a43
Motonosuke <67644458+Motonosuke@users.noreply.github.com>
2023-06-04 15:19:29 +0900

    chore: add next bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withBundleAnalyzer({
   i18n: { locales: ['ja'], defaultLocale: 'ja' },
   reactStrictMode: true,
   experimental: {
     esmExternals: false,
   },
-};
+});
